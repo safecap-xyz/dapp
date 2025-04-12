1367a9bdde1f504b7ec17ebd5407e3964b245bf0
Brian Yang <brianyang1@gmail.com>
2025-04-12 08:13:54 -0400

    feat: add tailwind, custom font, and initial landing page content

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,12 @@ function App() {
 
   return (
     <>
-      <div className='main-bg'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h1 className="font-faustina font-bold">Bold Heading</h1>
+    <h1 className="font-faustina font-bold">SafeCap.xyz</h1>
+    <div className='content-main'>
+      <h2 className='text-[46px]'>The Future of Crowdfunding is Here.</h2>
+      
+    </div>
 
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
