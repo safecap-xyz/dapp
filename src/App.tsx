709231c8bd5c5b201684d8e2d286f5e8bb4c5af6
Brian Yang <brianyang1@gmail.com>
2025-04-12 08:38:05 -0400

    feat: add features section to landing page

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,22 @@ function App() {
   return (
     <>
     <h1 className="font-faustina font-bold">SafeCap.xyz</h1>
-    <div className='content-main'>
-      <h2 className='text-[46px] font-faustina font-bold'>The Future of Crowdfunding is Here.</h2>
+    <div className='content-main mb-[32rem]'>
       
     </div>
 
+    <section className="content-full">
+  <div className="content-full-inner">
+      <h2 className='text-[46px] font-faustina font-bold'>The Future of Crowdfunding is Here.</h2>
+    <p className="content-text">
+      • Raise Funds Your Way: Accept ETH & ERC20 donations.<br/>
+      • Reward Support with NFTs: Automatically mint unique digital collectibles for backers.<br/>
+      • Transparent & Secure: Powered by audited smart contracts on the blockchain.<br/>
+      • All-or-Nothing Funding: Creator receives funds only if the goal is met.<br/>
+      • On-Chain Tracking: Verifiable progress and donation history.
+    </p>
+  </div>
+</section>
     </>
   )
 }
